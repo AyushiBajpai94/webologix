1926dd4692abfb67ac0b597181ab0be761f0d987
AyushiBajpai94 <ayushi.bajpai16@gmail.com>
2023-05-24 01:09:41 +0530

    done

--- Assignment_1/index.js
+++ Assignment_1/index.js
@@ -1,29 +1,67 @@
-const http = require("http");
-const fs = require("fs")
-const querystring= require("querystring")
-const url = require("url")
-http.createServer((req,res)=>{
-    if(req.url=="/file/download"){
-        const data=fs.readFileSync("./db.json","utf-8")
-       let jsonadata=JSON.parse(data)
-       
-       const parseUrl = url.parse(req.url)
-       const queryParams=querystring.parse(parseUrl.query)
-      
-       const param1 = queryParams.param1;
-  const param2 = queryParams.param2;
-  console.log(param1, param2);
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require('multer');
+const fs = require('fs');
 
-    //    if(req.query.id){
-    //     for(let i=0;i<=jsonadata.length-1;i++){
-    //         if(jsonadata[i].id==req.query.id){
-    //             res.end(JSON.stringify(jsonadata[i]))
-    //         }
-    //        }
-    //    }
-      
-        res.end(JSON.stringify(jsonadata))
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './Files');
+    },
+    filename: (req, file, cb) => {
+        const fileId = generateFileId(); 
+        const fileName = fileId + '_' + Date.now() + path.extname(file.originalname);
+        cb(null, fileName);
     }
-}).listen(7000,()=>{
-    console.log("Listening on port 7000")
-})
+});
+
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "upload.html"));
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+    console.log(req.file);
+    const fileId = generateFileId();
+    res.send("Image uploaded with fileId: " + fileId);
+});
+
+app.post("/Multi", upload.array("images", 3), (req, res) => {
+    console.log(req.files);
+    const fileId = generateFileId();
+    res.send("Images uploaded with fileId: " + fileId);
+});
+
+function generateFileId() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+app.get('/file/download', (req, res) => {
+    const fileID = req.query.id; 
+    if (!fileID) {
+        res.status(400).send('Missing fileID');
+        return;
+    }
+
+    const filePath = path.join(__dirname, 'Files', fileID); 
+
+  
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            res.status(404).send('File not found');
+        } else {
+            res.download(filePath, (err) => {
+                if (err) {
+                    console.error('Error while downloading file:', err);
+                } else {
+                    console.log('File downloaded successfully');
+                }
+            });
+        }
+    });
+});
+
+app.listen(8080, () => {
+    console.log("8080 is now working");
+});
